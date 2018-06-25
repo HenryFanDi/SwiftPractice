7a07da282fea1aa61715e02a916ca90b9389280e
Henry Fan <henry.fan@centraltrillion.com>
2018-06-26 07:59:34 +0800

    Project Initialize

---
+++ SwiftPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPractice
+//
+//  Created by HenryFan on 26/6/2018.
+//  Copyright © 2018 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
